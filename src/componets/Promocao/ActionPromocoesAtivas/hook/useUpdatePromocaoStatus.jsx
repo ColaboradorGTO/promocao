cb338d8297ab51d88200676e53478a86ebfa08b1
Myltiane Alves <myltiane.aux@gmail.com>
2025-07-31 08:41:48 -0300

    feat: add alteração de status da promoção de forma automática

---
+++ src/componets/Promocao/ActionPromocoesAtivas/hook/useUpdatePromocaoStatus.jsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react"
+import { put } from "../../../../api/funcRequest"
+import { getDataAtual, getDataTresMesesAtras } from "../../../../utils/dataAtual"
+import { useNavigate } from "react-router-dom"
+import axios from "axios";
+
+export const useUpdatePromocaoAtivaStatus = ({ dadosListaPromocao }) => {
+  const [ipUsuario, setIpUsuario] = useState("");
+  const [dataInicio, setDataInicio] = useState("");
+  const [dataFim, setDataFim] = useState("");
+  const [ usuarioLogado, setUsuarioLogado ] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const usuarioArmazenado = localStorage.getItem('usuario');
+
+    if (usuarioArmazenado) {
+      try {
+        const parsedUsuario = JSON.parse(usuarioArmazenado);
+        setUsuarioLogado(parsedUsuario);
+      } catch (error) {
+        console.error('Erro ao parsear o usuário do localStorage:', error);
+      }
+    } else {
+      navigate('/');
+    }
+  }, [navigate]);
+
+  useEffect(() => {
+    getIPUsuario();
+
+  }, [usuarioLogado]);
+
+  const getIPUsuario = async () => {
+    const response = await axios.get('http://ipwho.is/');
+    if (response.data) {
+      setIpUsuario(response.data.ip);
+    }
+    return response.data;
+  };
+
+  useEffect(() => {
+    const dataInicial = getDataAtual()
+    const dataFinal = getDataAtual()
+    setDataInicio(dataInicial)
+    setDataFim(dataFinal)
+  }, [])
+
+  const verificarPromocaoExpirada = async () => {
+    if(!dadosListaPromocao || dadosListaPromocao.length === 0) return
+
+    const dataAtual = new Date();
+    const promocoesExpiradas = [];
+
+    dadosListaPromocao.forEach(promocao => {
+      if(promocao?.DTHORAFIM) {
+        const dataFimPromocao = new Date(promocao.DTHORAFIM);
+
+        if(dataFimPromocao < dataAtual && promocao.STATIVO === 'True') {
+          promocoesExpiradas.push(promocao);
+        }
+      }
+    });
+
+    if(promocoesExpiradas.length > 0) {
+
+      for(const promocao of promocoesExpiradas) {
+         try {
+          await desativarPromocao(promocao.IDRESUMOPROMOCAOMARKETING);
+        } catch (error) {
+          console.error(`Erro ao desativar promoção ${promocao.IDRESUMOPROMOCAOMARKETING}:`, error);
+        }
+      }
+    }
+  }
+
+  const desativarPromocao = async (idPromocao) => {
+    
+    try {
+      const putData = {
+        STATIVO: 'False',
+        IDRESUMOPROMOCAOMARKETING: idPromocao,
+      };
+
+      const response = await put('/desativar-status-promocao', putData);
+
+      return response.data;
+    } catch (error) {
+      console.error('Erro ao Atualizar promoção:', error);
+      return null;
+    }
+  };
+
+    useEffect(() => {
+    if (dadosListaPromocao && dadosListaPromocao.length > 0) {
+      verificarPromocaoExpirada();
+    }
+  }, [dadosListaPromocao]);
+
+  return {
+    dataInicio,
+    setDataInicio,
+    dataFim,
+    setDataFim,
+    verificarPromocaoExpirada,
+    desativarPromocao
+  }
+}
