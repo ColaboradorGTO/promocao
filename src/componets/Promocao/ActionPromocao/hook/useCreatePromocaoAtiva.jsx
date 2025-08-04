f96466e1d668efc37ae3dcd2fb7ef85ecb61e614
Myltiane Alves <myltiane.aux@gmail.com>
2025-08-04 16:10:24 -0300

    feat: add ACTIONMAINPROMOCOESATIVAS

--- src/componets/Promocao/ActionPromocao/hook/useCreatePromocaoAtiva.jsx
+++ src/componets/Promocao/ActionPromocao/hook/useCreatePromocaoAtiva.jsx
@@ -521,11 +521,11 @@ export const useCreatePromocaoAtiva = ({ }) => {
         return;
       }
 
-      if (descricao.length < 20 || descricao.length > 200) {
+      if (descricao.length < 2 || descricao.length > 200) {
         Swal.fire({
           position: 'center',
           icon: 'error',
-          title: 'Descrição deve ter entre 20 e 200 caracteres!',
+          title: 'Descrição deve ter entre 2 e 200 caracteres!',
           customClass: {
             container: 'custom-swal',
           },
